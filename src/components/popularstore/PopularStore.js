c7c26512b764d863f0b11f7f3e382333e78d0b86
Vijay Mewada <vijaymewada2012@gmail.com>
2021-05-08 21:25:03 +0530

    popular store offer available

--- src/components/popularstore/PopularStore.js
+++ src/components/popularstore/PopularStore.js
@@ -4,67 +4,58 @@ import Amazon from "../../images/amazonlogo.jpg";
 import Flipkart from "../../images/flipartlogo.png";
 import Jiologo from "../../images/jiologo.png";
 import Freecharge from "../../images/freechargelogo.png";
-import { PopularStoreStyles } from "./PopularStoreStyles"
+import { PopularStoreStyles } from "./PopularStoreStyles";
 
 function PopularStore(props) {
-    const classes = PopularStoreStyles(props);
+  const classes = PopularStoreStyles(props);
   return (
     <Grid className={classes.root}>
       <Typography variant="h4">
         <span className={classes.headingtitle}>POPULAR</span> STORE
       </Typography>
-      <Grid item  container spacing={2}>
-      <Grid item  xl={2}>
-      
-      <Paper className={classes.paper}>
-      
-          
-
-
-        <img src={Amazon} className={classes.brand} alt="brand" />
-        
-        </Paper>
-       
+      <Grid item container spacing={2}>
+        <Grid item xl={2}>
+          <Paper className={classes.paper}>
+            <Typography variant="title" className={classes.couponavailable}>50 <br/> Coupons Available</Typography>
+            <img src={Amazon} className={classes.brand} alt="brand" />
+          </Paper>
         </Grid>
 
-        <Grid item  xl={2}>
-      <Paper className={classes.paper}>
-        <img src={Flipkart} className={classes.brand} alt="brand" />
-        </Paper>
+        <Grid item xl={2}>
+          <Paper className={classes.paper}>
+            <Typography variant="title" className={classes.couponavailable}>50 <br/> Coupons Available</Typography>
+            <img src={Flipkart} className={classes.brand} alt="brand" />
+          </Paper>
         </Grid>
 
-
-        <Grid item  xl={2}>
-      <Paper className={classes.paper}>
-        <img src={Jiologo} className={classes.brand} alt="brand" />
-        </Paper>
+        <Grid item xl={2}>
+          <Paper className={classes.paper}>
+            <Typography variant="title" className={classes.couponavailable}>50 <br/> Coupons Available</Typography>
+            <img src={Jiologo} className={classes.brand} alt="brand" />
+          </Paper>
         </Grid>
 
-
-        <Grid item  xl={2}>
-      <Paper className={classes.paper}>
-        <img src={Freecharge} className={classes.brand} alt="brand" />
-        </Paper>
+        <Grid item xl={2}>
+          <Paper className={classes.paper}>
+            <Typography variant="title" className={classes.couponavailable}>50 <br/> Coupons Available</Typography>
+            <img src={Freecharge} className={classes.brand} alt="brand" />
+          </Paper>
         </Grid>
 
-
-        <Grid item  xl={2}>
-      <Paper className={classes.paper}>
-        <img src={Amazon} className={classes.brand} alt="brand" />
-        </Paper>
+        <Grid item xl={2}>
+          <Paper className={classes.paper}>
+            <Typography variant="title" className={classes.couponavailable}>50 <br/> Coupons Available</Typography>
+            <img src={Amazon} className={classes.brand} alt="brand" />
+          </Paper>
         </Grid>
 
-
-        <Grid item  xl={2}>
-      <Paper className={classes.paper}>
-        <img src={Amazon} className={classes.brand} alt="brand" />
-        </Paper>
+        <Grid item xl={2}>
+          <Paper className={classes.paper}>
+            <Typography variant="title" className={classes.couponavailable}>50 <br/> Coupons Available</Typography>
+            <img src={Amazon} className={classes.brand} alt="brand" />
+          </Paper>
         </Grid>
-
-
-    
       </Grid>
-    
     </Grid>
   );
 }
--- src/components/popularstore/PopularStoreStyles.js
+++ src/components/popularstore/PopularStoreStyles.js
@@ -11,34 +11,53 @@ const PopularStoreStyles = makeStyles((theme) => ({
 
   },
   paper: {
+      zIndex:"10",
       margin:"10px",
     display: "flex",
     height: 175,
     padding: theme.spacing(2),
     textAlign: "center",
     color: theme.palette.text.secondary,
-
-    zIndex:"1000",
-    font: "55px Arial",
-  display: "flex",
-  textAlign: "center",
-  color: "white",
   background: "linear-gradient(to left, #fff 50%, rgba(99, 91, 91, 0.9) 50% ) right;",
   backgroundSize: "200%",
   transition: ".5s ease-out",
 
+
     '&:hover': {
+        
+         opacity:'0.91',
         backgroundPosition: "left",
+        backgroundColor: "#008CBA",
+        cursor:"pointer",
+        "& $brand": {
+          opacity:'0.1',
+        
+        },
+        "& $couponavailable": {
+          position:"absolute",
+          fontSize:"20px",
+          color:"white",
+          textAlign:"center",
+          display:'block',
+          
+        
+        }
     },
+  
+    
+
   },
+  couponavailable :{
+    display:"none"
+  },
+
+ 
   brand: {
+    height: 175,
     width: "150px",
     margin: "auto",
     zIndex:"100",
   },
- hover:{
-    
- },
-
+ 
 }));
 export { PopularStoreStyles };
