6cdc9a9ad6bb8eaa169da9dc14c363384ed225de
Vijay Mewada <vijaymewada2012@gmail.com>
2021-05-10 19:20:24 +0530

    couponupload file work

--- src/pages/uploadcoupon/UploadCoupon.js
+++ src/pages/uploadcoupon/UploadCoupon.js
@@ -1,13 +1,95 @@
-import { Grid } from '@material-ui/core'
-import React from 'react'
+import {
+  FormControl,
+  Grid,
+  Input,
+  Select,
+  InputLabel,
+  FormHelperText,
+  TextField,
+  TextareaAutosize,
+} from "@material-ui/core";
+import { UploadCouponStyles } from "./UploadCouponStyles";
+import React from "react";
+import {
+    MuiPickersUtilsProvider,
+    KeyboardTimePicker,
+    KeyboardDatePicker,
+  } from '@material-ui/pickers';
+// import DateFnsUtils from '@date-io/date-fns';
 
-function UploadCoupon() {
-    return (
-        
-           <Grid>
-               
-           </Grid>
-    )
+function UploadCoupon(props) {
+  const classes = UploadCouponStyles(props);
+
+  const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+
+  
+  return (
+    <Grid style={{display:'flex', margin:'25px'}}>
+      <FormControl style={{margin:"auto"}}>
+      <FormControl>
+        <TextField
+          id="outlined-Discount Title-input"
+          label="Discount Title"
+          type="Discount Title"
+          variant="outlined"
+        />
+        <FormHelperText id="my-helper-text">
+          Ex. Flat 50 off & 50% off
+        </FormHelperText>
+        </FormControl>
+        <br/>
+        <FormControl variant="outlined">
+        <TextareaAutosize
+          className={classes.txtarea}
+          placeholder="Discription of a offer"
+          rowsMin={5}
+        />
+        <FormHelperText id="my-helper-text">
+          Ex. Upto Rs. 50 Amazon Pay Cashback on Payments done via Amazon Pay
+          UPI (New User)
+        </FormHelperText>
+        </FormControl>
+        <br/>
+
+        <FormControl variant="outlined">
+        <InputLabel htmlFor="outlined-age-native-simple">Offer Type</InputLabel>
+        <Select
+          native
+        //   value={state.age}
+        //   onChange={handleChange}
+          label="offertype"
+        >
+          <option aria-label="None" value="" />
+          <option > For All User</option>
+          <option >User Specific</option>
+        </Select>
+        </FormControl>
+
+        <FormControl variant="outlined">
+        <MuiPickersUtilsProvider>
+        <KeyboardDatePicker
+          margin="normal"
+          id="date-picker-dialog"
+          label="Date picker dialog"
+          format="MM/dd/yyyy"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+        </MuiPickersUtilsProvider>
+        </FormControl>
+
+
+      </FormControl>
+    </Grid>
+  );
 }
 
-export default UploadCoupon
+export default UploadCoupon;
---
+++ src/pages/uploadcoupon/UploadCouponStyles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const UploadCouponStyles = makeStyles({
+  txtarea: {
+    resize: "none",
+    height: "150px",
+  },
+});
+export { UploadCouponStyles };
