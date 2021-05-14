888870680a7229db97321963b46175fb52b1e0d1
dev-dheeraj <67103189+dev-dheeraj@users.noreply.github.com>
2021-05-14 15:45:34 +0530

    Added dynamic category filter on side bar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,50 +1,67 @@
 import { Card, Grid, Typography } from "@material-ui/core";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { SidebarStyles } from "./Sidebarstyles";
 import Checkbox from "@material-ui/core/Checkbox";
+import { get } from "../../api/serverRequest";
 
 function Sidebar(props) {
   const classes = SidebarStyles(props);
-  const [checked, setChecked] = useState(false);
+  const [checkBoxStatus, setCheckBoxStatus] = useState(false);
+  const [categoryList, setcategoryList] = useState([]);
+  const [categoryIds, setCategoryIds] = useState([]);
 
   const handleChange = (event) => {
-    setChecked(event.target.checked);
+    if (event.target.checked == true && event.target.value) {
+      setCategoryIds(oldIds => [...oldIds, event.target.value]);
+    } else {
+      setCategoryIds(categoryIds.filter(item => item !== event.target.value));
+    }
+    // setTimeout(() => {
+    //   console.log(categoryIds);
+    // }, 500);
+    setCheckBoxStatus(event.target.checked);
   };
+  useEffect(() => {
+    console.log(categoryIds);
+    
+  }, [categoryIds])
+
+
+  // componennt didmount 
+  useEffect(async () => {
+    // get all category list
+    let res = await get("/coupon/getAllCategory");
+    if (res.data && res.data.content && res.data.content.length) {
+      setcategoryList(res.data.content)
+    }
+  }, [])
+
+  // render filter category on sidebar
+
+  const renderCategoryFilter = () => {
+    return categoryList && categoryList.map((item, ind) => {
+      return <Grid className={classes.rechargectgry}>
+        <Typography variant="title" >
+          <Checkbox
+            color="default"
+            inputProps={{ "aria-label": "checkbox with default color" }}
+            value={item.id}
+            onChange={(e) => handleChange(e)}
+          />  {item.name}
+        </Typography>
+      </Grid>
+
+    })
+  }
 
   return (
     <Grid>
       <Grid xs={12} sm={12} md={12} lg={12} xl={12}>
         <Card className={classes.card}>
-        <Typography variant="h6">Categories</Typography>
+          <Typography variant="h6">Categories</Typography>
 
-<Grid className={classes.rechargectgry}>
-<Typography variant="title" >
-  <Checkbox
-    
-    color="default"
-    inputProps={{ "aria-label": "checkbox with default color" }}
-  />  Mobile Recharge
-</Typography>
-</Grid>
-<Grid className={classes.rechargectgry}>
-<Typography variant="title" >
-  <Checkbox
-    
-    color="default"
-    inputProps={{ "aria-label": "checkbox with default color" }}
-  />  DTH Recharge
-</Typography>
-</Grid>
-
-<Grid className={classes.rechargectgry}>
-<Typography variant="title" >
-  <Checkbox
-    
-    color="default"
-    inputProps={{ "aria-label": "checkbox with default color" }}
-  />  Bill Payment
-</Typography>
-        </Grid>
+          {/*  render category filter on side bar */}
+          {renderCategoryFilter()}
         </Card>
 
 
