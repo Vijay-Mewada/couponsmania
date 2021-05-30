123083f9e2f8938c61d57669afeee82df52a1153
dev-dheeraj <dheerajnagar1998@gmail.com>
2021-05-30 17:21:10 +0530

    chenged env file

--- src/api/serverRequest.js
+++ src/api/serverRequest.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 // const baseUrl = "https://oninecoponsapp.herokuapp.com/api";// live url
-export const serverImageUrl = 'http://52.15.186.222:8000/images'
-// export const serverImageUrl = 'http://127.0.0.1:8000/images'
+// export const serverImageUrl = 'http://52.15.186.222:8000/images'
+export const serverImageUrl = 'http://127.0.0.1:8000/images'
 // export const serverImageUrl = 'https://oninecoponsapp.herokuapp.com/images'
 // export const serverUrl = 'https://oninecoponsapp.herokuapp.com'
-const serverUrl = "http://52.15.186.222:8000";
-// const serverUrl = "http://127.0.0.1:8000";
+// const serverUrl = "http://52.15.186.222:8000";
+const serverUrl = "http://127.0.0.1:8000";
 export const baseUrl = `${serverUrl}/api`;
 
 export const get = async (url) => {
